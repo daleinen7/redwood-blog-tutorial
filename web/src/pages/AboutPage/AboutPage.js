70482c0c9891225433789551391021474d97b7b6
daleinen7 <daleinen@gmail.com>
2021-06-22 16:18:27 -0500

    blogs appear

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,5 +1,3 @@
-import { Link, routes } from '@redwoodjs/router'
-
 const AboutPage = () => {
   return (
     <>
@@ -8,7 +6,6 @@ const AboutPage = () => {
           This site was created to demonstrate my mastery of Redwood: Look on my
           works, ye mighty, and despair!
         </p>
-        <Link to={routes.home()}>Return home</Link>
       </main>
     </>
   )
